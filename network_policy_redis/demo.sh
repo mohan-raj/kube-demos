f221c5286fbe085a4f0ebbc74006655f085e0eb9
Casey Davenport <casey@tigera.io>
2016-11-03 18:41:07 -0700

    Update demo, include vagrant cluster.

--- network_policy_redis/demo.sh
+++ network_policy_redis/demo.sh
@@ -2,28 +2,33 @@
 
 . $(dirname ${BASH_SOURCE})/../util.sh
 
+desc "Install Calico + kubedns"
+run "kubectl apply -f $(relative calico.yaml)"
+run "kubectl apply -f $(relative kubedns.yaml)"
+
+desc "Create a Namespace for the demo"
+run "kubectl create ns demos"
+
 desc "Run the redis datastore"
 run "cat $(relative redis-rc.yaml)"
-run "kubectl --namespace=demos create -f $(relative redis-rc.yaml)"
-
+run "kubectl create -f $(relative redis-rc.yaml)"
 
 desc "Run a service for redis"
 run "cat $(relative redis-svc.yaml)"
-run "kubectl --namespace=demos create -f $(relative redis-svc.yaml)"
-run "kubectl --namespace=demos describe rc redis"
+run "kubectl create -f $(relative redis-svc.yaml)"
+run "kubectl describe rc redis --namespace=demos"
 
 desc "Use a simple Python app to access redis"
-run "cat $(relative app.py)"
+run "cat $(relative app/app.py)"
 
 desc "Run a replication controller for the frontend"
 run "cat $(relative frontend-rc.yaml)"
-run "kubectl --namespace=demos create -f $(relative frontend-rc.yaml)"
+run "kubectl create -f $(relative frontend-rc.yaml)"
 
 desc "Run a service for the frontend"
 run "cat $(relative frontend-svc.yaml)"
-run "kubectl --namespace=demos create -f $(relative frontend-svc.yaml)"
-
-run "kubectl --namespace=demos describe rc frontend"
+run "kubectl create -f $(relative frontend-svc.yaml)"
+run "kubectl describe rc frontend --namespace=demos"
 
 tmux new -d -s my-session \
     "$(dirname ${BASH_SOURCE})/split1_lhs.sh" \; \
--- network_policy_redis/split1_lhs.sh
+++ network_policy_redis/split1_lhs.sh
@@ -5,9 +5,12 @@
 IP=$(kubectl --namespace=demos get svc frontend \
         -o go-template='{{.spec.clusterIP}}')
 
-run "ssh -i /tmp/key core@$SSH_NODE  '\\
+KEY="/Users/casey/.vagrant.d/insecure_private_key"
+SSH_NODE=172.18.18.101
+
+run "ssh -i $KEY core@$SSH_NODE  '\\
     while true; do \\
-        curl --connect-timeout 1 -s $IP && echo || echo \"(timeout)\"; \\
+        curl --connect-timeout 1 -s $IP && echo || echo \"Failed to access frontend\"; \\
         sleep 1; \\
     done \\
     '"
